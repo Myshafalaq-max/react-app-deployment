80822c52c0727cc03b905e4c054c1d1d980a79b4
Myshafalaq-max <myshafalaq@gmail.com>
2026-08-11 12:03:02 +0530

    Fix Docker Hub login and image configuration

--- build.sh
+++ build.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-#login into DockerHub:
-docker login -u $DOCKER_USERNAME -p $DOCKER_PASS
+# Login to Docker Hub securely
+printf '%s\n' "$DOCKER_PASS" | docker login --username "$DOCKER_USERNAME" --password-stdin
 
-#stopping existing container:
-docker stop react
-docker rm react
+# Stop and remove existing container if it exists
+docker rm -f react 2>/dev/null || true
 
-#building a image:
+# Build Docker image
 docker build -t react-ci/cd .
 
-#running a container from the created image:
-docker run -d -it --name react -p 80:80 react-ci/cd
+# Run the container
+docker run -d --name react -p 80:80 react-ci/cd
 
-#pushing the image to dockerhub:
-docker tag react-ci/cd naveen712/react-app:ci-cd
-docker push naveen712/react-app:ci-cd
+# Tag the image for your Docker Hub repository
+docker tag react-ci/cd myshatech/react-app:ci-cd
+
+# Push the image to Docker Hub
+docker push myshatech/react-app:ci-cd
